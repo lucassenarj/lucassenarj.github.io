8793095a3d594623ec956df6e19e1cf3d1a4d55b
Lucas Sena <lucassena.rj@gmail.com>
2018-11-22 00:21:12 -0200

    Ajusts Settings

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/blog/service-worker.js').then(() => {
+  navigator.serviceWorker.register('/site/service-worker.js').then(() => {
     console.log('Service Worker registered!');
   });
 }
